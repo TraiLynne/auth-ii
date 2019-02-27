e37b1ddd2175336e63d3b4e3cd33164ef9eae185
Trai Lynne <trailynne@gmail.com>
2019-02-27 14:37:26 -0500

    Define Users Table Generate migration file with Knex

---
+++ db/migrations/20190227143251_add_users_table.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+
+        tbl
+            .string('username')
+            .notNullable()
+            .unique();
+
+        tbl
+            .string('password')
+            .notNullable();
+
+        tbl
+            .string('department');
+
+        tbl
+            .timestamp('createdAt')
+            .defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+};
